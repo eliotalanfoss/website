759bc8f638804bfd91187f462d20d0ef807d55df
Eliot Alan Foss <eliotfoss@gmail.com>
2023-12-08 22:22:32 -0800

    Change metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Eliot's Website",
+	url: "https://eliotfoss.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Eliot Alan Foss",
+		email: "eliotfoss@gmail.com",
+		url: "https://eliotfoss.com/about-me/"
 	}
 }
